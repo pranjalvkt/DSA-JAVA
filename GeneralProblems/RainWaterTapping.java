18327f40b4511de60b01f962d9f99013d4f631e4
Pranjal Tripathi <pranjalvktripathi@gmail.com>
2021-05-31 16:33:56 +0530

    Added RainWaterTrapping.java

---
+++ GeneralProblems/RainWaterTapping.java
@@ -0,0 +1,33 @@
+/*
+*   @author
+*   Pranjal Tripathi
+*/
+
+package GeneralProblems;
+
+public class RainWaterTapping {
+    public static void main(String[] args) {
+        int[] arr = {3, 1, 2, 4, 0, 1, 3, 2};
+        System.out.println(calculateRainWater(arr));
+    }  
+    static int calculateRainWater(int[] arr) {
+        int n = arr.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+
+        left[0] = arr[0];
+        right[n-1] = arr[n-1];
+
+        for (int i = 1; i < n; i++) {
+            left[i] = Math.max(left[i-1], arr[i]);
+        }
+        for (int i = n-2; i >= 0; i--) {
+            right[i] = Math.max(right[i+1], arr[i]);
+        }
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans += (Math.min(left[i], right[i]) - arr[i]);
+        }
+        return ans;
+    }
+}

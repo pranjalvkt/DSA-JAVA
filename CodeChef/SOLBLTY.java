fa7d5799d528fed49319ef1837d83d8642e816ae
Pranjal Tripathi <pranjalvkt.srmcem@gmail.com>
2021-09-15 19:10:01 +0530

    Add new

--- CodeChef/SOLBLTY.java
+++ CodeChef/SOLBLTY.java
@@ -2,86 +2,79 @@ package CodeChef; // REMOVE PACKAGE
 import java.util.*;
 import java.io.*;
 
-public class SOLBLTY { // CHANGE CLASS DECLARATION TO--> "class CodeChef"
+public class SOLBLTY {// CHANGE CLASS DECLARATION TO--> "class CodeChef"
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader() {
+            br=new BufferedReader(new InputStreamReader(System.in));
+        }
+        String next() {
+            while(st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+        String nextLine(){
+            String str = "";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+    static class FastWriter {
+        private final BufferedWriter bw;
+
+        public FastWriter() {
+            this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+
+        public void print(Object object) throws IOException {
+            bw.append("" + object);
+        }
+
+        public void println(Object object) throws IOException {
+            print(object);
+            bw.append("\n");
+        }
+
+        public void close() throws IOException {
+            bw.close();
+        }
+    }
     public static void main(String[] args) {
         try {
             FastReader fs = new FastReader();
-            /* WRITE YOUR CODE HERE */
+            FastWriter out = new FastWriter();
             int t = fs.nextInt();
-            while (t-- > 0) {
+            while(t-- > 0){
+                /* WRITE YOUR CODE HERE */
                 int x = fs.nextInt();
                 int a = fs.nextInt();
                 int b = fs.nextInt();
 
                 int res = (a + (100 - x) * b) * 10;
-                System.out.println(res);
+                out.println(res);
             }
-            
-
-        } catch(Exception e) {
+            out.close();
+        } catch (Exception e) {
             return;
         }
-        
     }
-static class FastReader {
-    BufferedReader br;
-    StringTokenizer st;
- 
-    public FastReader()
-    {
-        br = new BufferedReader(
-        new InputStreamReader(System.in));
-    }
- 
-    String next()
-    {
-          while (st == null || !st.hasMoreElements()) {
-              try {
-                  st = new StringTokenizer(br.readLine());
-              }
-              catch (IOException e) {
-                  e.printStackTrace();
-              }
-          }
-          return st.nextToken();
-    }
- 
-    int nextInt() { return Integer.parseInt(next()); }
- 
-    long nextLong() { return Long.parseLong(next()); }
- 
-    double nextDouble()
-    {
-        return Double.parseDouble(next());
-    }
-
-    float nextFloat()
-    {
-        return Float.parseFloat(next());
-    }
- 
-    String nextLine()
-    {
-        String str = "";
-            try {
-                str = br.readLine();
-            }
-            catch (IOException e) {
-                e.printStackTrace();
-            }
-        return str;
-    }
-
-    int[] readArray(int n) {
-        int[] a=new int[n];
-        for (int i=0; i<n; i++) a[i]=nextInt();
-        return a;
-    }
-
-    long[] readArrayLong(int n) {
-        long[] a=new long[n];
-        for (int i=0; i<n; i++) a[i]=nextLong();
-        return a;
-    }
-}
 }
